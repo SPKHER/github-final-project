6a3d5aa041d88bc17cc8973394a4369840f28c01
SPKHER <168692570+SPKHER@users.noreply.github.com>
2024-11-25 18:37:38 -0500

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -11,7 +11,7 @@
    # Input:
    # p, principal amount
    # t, time period in years
-   # r, annual rate of interest
+   # r, annual rate of interest 
 
    # Output:
    # simple interest = p*t*r
